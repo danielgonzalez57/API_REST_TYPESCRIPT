3a53910133bd2d8a4f3aad66fb957be75a1a0b36
Daniel Gonzalez <da.gonzalez@DAKA.LOCAL>
2024-11-13 14:01:03 -0400

    13 11 2024

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -6,18 +6,24 @@ import { encrypt } from "../utils/bcrypt.handle";
 // REGISTER
 const registerNewUser = async ({Email, Password, Nombre, Departament, ID_Rol}: Omit<User, 'ID_User'>) => {
 
+    // Chequea si hay un usuario con el mismo email
     const isCheck = await sequelize.models.modelUser.findOne({where:{Email: Email}});
     if(isCheck) return "ALREADY_USER";
 
+    // Hashea el password
     const passHash = await encrypt(Password);
 
+    // Regista el usuario con el password hasheado
     const registerNewUser = await sequelize.models.modelUser.create({Email, Password:passHash , Nombre, Departament, ID_Rol});
     return registerNewUser;
 };
 
 // LOGIN
-const loginUser = async () => {
+const loginUser = async ({Email, Password}: Auth) => {
+    const isCheck = await sequelize.models.modelUser.findOne({where:{Email: Email}});
+    if(!isCheck) return "NOT_FOUND_USER";
 
+    // const passwordHash = isCheck.Password
 };
 
 export { registerNewUser, loginUser };
